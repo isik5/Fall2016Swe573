3b0682aa741429ce53511c50cdac9a1b076b8a5d
Isik Mater <isikmater@gmail.com>
2016-12-04 05:33:43 +0300

    initial commit for Fall2016Swe573

--- tracker/caloriewatcher/views.py
+++ tracker/caloriewatcher/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import (render_to_response, render)
 from .forms import FoodSearchForm
 from caloriewatcher import fcd_api
+from django.http import HttpResponseRedirect
+from django.contrib import auth
+from django.contrib.auth.views import logout
+from django.template.context_processors import csrf
 
 
 def home(request):
@@ -29,7 +33,36 @@ def food_search(request):
             else:
                 pass
 
-            # if a GET (or any other method) we'll create a blank form
+                # if a GET (or any other method) we'll create a blank form
     else:
         ctx['form'] = FoodSearchForm()
     return render(request, 'food_search.html', ctx)
+
+
+def login(request):
+    c = {}
+    c.update(csrf(request))
+    return render_to_response('login.html', c)
+
+
+def auth_view(request):
+    username = request.POST.get('username', '')
+    password = request.POST.get('password', '')
+    user = auth.authenticate(username=username, password=password)
+
+    if user is not None:
+        auth.login(request, user)
+        return HttpResponseRedirect('/accounts/loggedin')
+    else:
+        return HttpResponseRedirect('/accounts/invalid')
+
+def loggedin(request):
+    return render_to_response('loggedin.html',
+                              {'full_name': request.user.username})
+
+def invalid_login(request):
+    return render_to_response('invalid_loggedin.html')
+
+def logout(request):
+    auth.logout(request)
+    return render_to_response('logout.html')
