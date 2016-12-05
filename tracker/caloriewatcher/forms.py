813b30b0ffe23a333a02230c5a5fed8b32d4a029
Isik Mater <isikmater@gmail.com>
2016-12-05 03:54:33 +0300

    Registration form extended

--- tracker/caloriewatcher/forms.py
+++ tracker/caloriewatcher/forms.py
@@ -9,16 +9,20 @@ class FoodSearchForm(forms.Form):
 
 class MyRegistrationForm(UserCreationForm):
     email = forms.EmailField(required=True)
+    gender = forms.CharField(required=True)
+    born = forms.DateField(required=False)
+    height = forms.FloatField(required=False)
+    weight = forms.IntegerField(required=False)
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password1', 'password2')
+        fields = ('username', 'email', 'password1', 'password2', 'gender', 'born', 'height', 'weight')
 
     def save(self, commit=True):
         user = super(MyRegistrationForm, self).save(commit=False)
         user.email = self.cleaned_data['email']
         # user.set_password(self.cleaned_data['password1'])
-
+        user.born = self.cleaned_data['born']
         if commit:
             user.save()
 
--- tracker/caloriewatcher/models.py
+++ tracker/caloriewatcher/models.py
@@ -6,6 +6,15 @@ class UserSpecs(models.Model):
     user = models.ForeignKey(User)
     height = models.IntegerField('user height in cm')
     weight = models.FloatField('user weight in kg')
+    born = models.DateTimeField('mm/yyyy')
+
+
+class Foo(models.Model):
+    GENDER_CHOICES = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+    )
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES, default='M', null=True)
 
 
 class Exercise(models.Model):
