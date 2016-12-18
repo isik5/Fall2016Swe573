d5431781508123c69daa6ae806958f2f03ac206d
Isik Mater <isikmater@gmail.com>
2016-12-19 02:08:18 +0300

    front-end implementation

--- tracker/caloriewatcher/fcd_api.py
+++ tracker/caloriewatcher/fcd_api.py
@@ -2,7 +2,7 @@ from django.conf import settings
 import requests
 
 FCD_API_URL = "http://api.nal.usda.gov/ndb/{0}/?format=json&api_key={1}"
-FCD_API_KEY = settings.FCD_API_KEY
+FCD_API_KEY = gBxQKntXgGDh2Jxj4um8bBNayqIvbFGMxC1QWU6F
 
 
 def get_foods(kw):
